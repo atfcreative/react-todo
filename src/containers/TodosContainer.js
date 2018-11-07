a04a5dbe8c9e174df259a022bb8d025210aad746
atfcreative <atfcreative@gmail.com>
2018-11-07 12:20:07 -0800

    initial commit, adds API connection

---
+++ src/containers/TodosContainer.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import TodoModel from '../models/Todo';
+import Todos from '../components/Todos';
+
+class TodosContainer extends Component {
+    
+    constructor(){
+        super()
+        this.state = {
+            todos:[]
+        }
+    }
+    
+    componentDidMount(){
+        this.fetchData()
+    }
+    
+    fetchData(){
+        TodoModel.all().then((res) => {
+            this.setState ({
+                todos: res.data.todos,
+                todo: ''
+            })
+        })
+    }
+    
+    render() { 
+        TodoModel.all().then((res) => {
+            console.log(res);
+        })
+    
+    return (
+        <div className="todosComponent">
+            <Todos 
+            todos={this.state.todos} />
+        </div>
+        );
+    }
+}
+ 
+export default TodosContainer;
